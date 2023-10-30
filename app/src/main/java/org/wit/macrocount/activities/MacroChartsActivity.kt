0efb644af6c1c523ee5cf63f6daa634d091bf3aa
Darragh Conneely <darraghc23@gmail.com>
2023-10-30 21:15:25 +0000

    progress bar logic

--- app/src/main/java/org/wit/macrocount/activities/MacroChartsActivity.kt
+++ app/src/main/java/org/wit/macrocount/activities/MacroChartsActivity.kt
@@ -5,16 +5,50 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
 import org.wit.macrocount.R
+import org.wit.macrocount.main.MainApp
+import org.wit.macrocount.models.UserModel
+import org.wit.macrocount.models.UserRepo
+import org.wit.macrocount.helpers.calcBmr
+import org.wit.macrocount.models.MacroCountModel
+import timber.log.Timber
 
 class MacroChartsActivity : AppCompatActivity() {
+    lateinit var app : MainApp
     private lateinit var caloriesProgressBar: ProgressBar
+    private lateinit var userRepo: UserRepo
+    private var user: UserModel? = null
+    private var calorieGoal: Double = 0.0
+    private var dailyCalories: Int = 0
+    private var userMacros: List<MacroCountModel>? = null
+    private var caloriesProgress: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_charts)
 
+        app = application as MainApp
+        userRepo = UserRepo(applicationContext)
+
+        val currentUserId = userRepo.userId
+        if (currentUserId != null) {
+            user = app.users.findById(currentUserId.toLong())
+        }
+
+        if (user != null && userMacros != null) {
+            calorieGoal = calcBmr(user!!.weight.toInt(), user!!.height.toInt(), user!!.age.toInt(), user!!.goal)
+            Timber.i("calorieGoal: $calorieGoal")
+            userMacros = app.macroCounts.findByUserId(user!!.id)
+            Timber.i("userMacros: $userMacros")
+            dailyCalories = userMacros!!.sumOf { it.calories.toInt() }
+            Timber.i("dailyCalories: $dailyCalories")
+            caloriesProgress = (dailyCalories / calorieGoal) * (100 / 1)
+            Timber.i("userMacros: $caloriesProgress")
+        }
+
+
+
         caloriesProgressBar = findViewById(R.id.caloriesProgressBar)
-        caloriesProgressBar.setProgress(20)
+        caloriesProgressBar.progress = caloriesProgress.toInt()
 
     }
 
--- app/src/main/java/org/wit/macrocount/helpers/AnalyticsHelper.kt
+++ app/src/main/java/org/wit/macrocount/helpers/AnalyticsHelper.kt
@@ -4,14 +4,20 @@ import android.content.Context
 import org.wit.macrocount.models.UserModel
 import timber.log.Timber.Forest.e
 
-fun calcBmr(weight: Int, height: Int, age: Int): Double {
-    return 88.362 + (13.397 * weight) + (4.799 * height ) - (5.677 * age)
-}
 
-fun calcWeightLossGoal(bmr: Double): Double {
-    return bmr - 600
-}
 
-fun calcWeightGainGoal(bmr: Double): Double {
-    return bmr + 600
+fun calcBmr(weight: Int, height: Int, age: Int, goal: String): Double {
+    val bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+
+    return when (goal) {
+        "lose" -> {
+            bmr - 600.0
+        }
+        "gain" -> {
+            bmr + 600.0
+        }
+        else -> {
+            bmr
+        }
+    }
 }
