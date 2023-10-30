9a62899cf14bcd5dae8da267199479ba1f624e38
Darragh Conneely <darraghc23@gmail.com>
2023-10-30 17:22:41 +0000

    added pie chart to data layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
 }
 
 dependencies {
@@ -50,6 +51,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.github.lecho:hellocharts-library:1.5.8@aar")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
